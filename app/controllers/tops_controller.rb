a5fa7b30c3c9e7d291c1273c5963a4ec3d51b611
kzkio114 <kzkio114@gmail.com>
2024-09-26 18:44:20 +0900

    修正ずみいいね

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,4 +2,6 @@ class TopsController < ApplicationController
   def index
     @fonts = Font.all # フォントの一覧を取得
   end
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
     end
   end
 
-
   root "tops#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
